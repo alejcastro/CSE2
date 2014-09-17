716af9b118f8bb6b22bd79ccfbd471d421a1c9d3
alejcastro <ajc217@lehigh.edu>
2014-09-17 01:27:33 +0000

    updated hw03

--- hw03/root.java
+++ hw03/root.java
@@ -1,10 +1,9 @@
 //////////////////////////////////////////////////////////////////////
 // Alejandro Castro 
-// lab03
-//BigMac Program
-// this program will compute the cost of buying BigMacs. this will compute the co
-//cost per big mac, and the percentage tax. this wil depend on the state and 
-//finally the total cost. we will use the scanner class to accomplish it.
+// hw03
+//Root Program
+// this program will compute the cube root of any number you choose by estimating it
+// for accomplishing it we will use a scanner to read the number inserted
 //for using the scanner this should be the first statement in your program:
 import java.util.Scanner;
 //define the class
@@ -13,19 +12,28 @@ public class root{
 public static void main(String[] args) {
 Scanner myScanner;
 myScanner=new Scanner(System.in);
+// this will let the user enter his number for the program to read and estimate the
+// cube root
 
 System.out.print("Enter a double and i print its cube root: ");//this will allow the user to 
-    //enter the cost per big mac
-    double x= myScanner.nextDouble();
-double guess1,guess2,guess3,guess4,guess5;
-guess1=x/3;
+ // to enter the number   
+double x= myScanner.nextDouble();// we named x the user's number
+double guess1,guess2,guess3,guess4,guess5;// we declare all the variables
+guess1=x/3;// this is our first guess
+// with our first guess we  plug it in  on the following formula to create another
+// guess and with that guess another guess this will allow us to get a better 
+// estimation of the real number
 guess2=(guess1*guess1*guess1+x)/(3*guess1*guess1);
 guess3=(guess2*guess2*guess2+x)/(3*guess2*guess2);
 guess4=(guess3*guess3*guess3+x)/(3*guess3*guess3);
 guess5=(guess4*guess4*guess4+x)/(3*guess4*guess4);
+// with 5 diferent guess our number is close enough with the real value?
+// lets test it!
+// print  the result of your estimation
 System.out.println("the cube root is "+guess5+" ="+guess5+"*"+guess5+"*"+guess5+"="+
-guess5*guess5*guess5+"etimated");
-}
-}
+guess5*guess5*guess5+" etimated");// for veryfing your answer increased it to the 3rd power
+// to see if it is similar to the user's number
+}// end class
+}// end main method
 
     
